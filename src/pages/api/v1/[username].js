b446eaa8aad451618660d5304726d6f11276c47b
Thomas Dohmke <thomas@dohmke.de>
2019-12-23 07:59:46 -0800

    Reduce cache to 60.

--- src/pages/api/v1/[username].js
+++ src/pages/api/v1/[username].js
@@ -3,6 +3,6 @@ import { fetchDataForAllYears } from '../../../utils/api/fetch'
 export default async (req, res) => {
   const { username, format } = req.query;
   const data = await fetchDataForAllYears(username, format);
-  res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate')
+  res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
   res.json(data);
 }
