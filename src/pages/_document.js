ed9f03c99ca53d22b51fa63be67531e0a1649c36
Thomas Dohmke <thomas@dohmke.de>
2021-12-06 11:04:26 -0800

    doh

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,17 +1,17 @@
 import React from "react";
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 export default class extends Document {
   render() {
     return (
-      <>
+      <Html>
         <Head>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-      </>
+      </Html>
     );
   }
 }
